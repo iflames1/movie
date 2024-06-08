ee97a64592bcccc66ffb15b8a366d2e7c77bdb26
Flames <isaacomenuche@gmail.com>
2024-06-08 17:42:13 +0100

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Velit esse incididunt nulla occaecat. Irure amet exercitation irure
+        dolore minim ea laborum. Consequat anim dolore enim veniam excepteur.
+        Pariatur et dolore enim deserunt ut ea adipisicing exercitation
+        voluptate officia ea. Veniam voluptate nostrud do labore excepteur
+        occaecat consectetur reprehenderit ut.Id ea tempor excepteur cillum ut
+        eu velit irure. Dolor dolor ullamco ad ipsum elit incididunt elit aliqua
+        ex sint. Dolore cupidatat id labore aute. Voluptate cupidatat voluptate
+        minim officia non reprehenderit et. Ipsum occaecat in aliqua Lorem ipsum
+        ad Lorem sunt ipsum laborum voluptate dolore. Nisi consectetur veniam
+        quis qui consequat culpa eu eiusmod aliquip ea. Consectetur et occaecat
+        do ipsum dolor culpa. Cupidatat commodo laborum officia fugiat magna
+        voluptate id laborum. Exercitation eu incididunt id quis magna Lorem
+        reprehenderit proident ut excepteur voluptate dolor ad amet. Eiusmod
+        magna in adipisicing ea cupidatat elit et irure laboris anim aliquip
+        aliquip duis nostrud. Dolore esse deserunt officia deserunt cillum amet
+        cillum sunt incididunt in nostrud velit consequat Lorem. Minim anim
+        proident voluptate ut elit cillum nostrud sunt. Elit cillum fugiat elit
+        dolor duis esse labore ea ullamco in id ea. Deserunt elit tempor magna
+        officia dolore.
+      </p>
+      <p>
+        Cillum incididunt incididunt sint irure do laborum laboris. Incididunt
+        esse amet laborum laborum nisi ea duis. Elit dolore minim occaecat culpa
+        do elit reprehenderit eiusmod.
+      </p>
+    </div>
+  );
+}
