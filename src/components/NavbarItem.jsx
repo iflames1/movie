38e3d4ee3ac3099a75311c40f2a6e43ecd1224d2
Flames <isaacomenuche@gmail.com>
2024-06-10 01:06:40 +0100

    dissable suspence

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -7,17 +7,15 @@ export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <Suspense className="">
-      <Link
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        } `}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </Suspense>
+    <Link
+      className={`hover:text-amber-600 font-semibold ${
+        genre === param
+          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+          : ""
+      } `}
+      href={`/?genre=${param}`}
+    >
+      {title}
+    </Link>
   );
 }
